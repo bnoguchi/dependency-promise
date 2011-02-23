5c279b9e18c5bb48f48e3f31476203ba7cd17e63
bnoguchi <brian.noguchi@gmail.com>
2011-02-23 13:47:21 -0800

    Inital commit. Complete set of code.

---
+++ dependency_promise.js
@@ -0,0 +1,73 @@
+var dependencyPromise = module.exports = {
+  dependsOn: function (event, children) {
+    var events = this.events || (this.events = {})
+      , e = events[event] || (events[event] = {})
+      , childs = e.children || (e.children = [])
+      , i = children.length, cevents, ce
+      , parents, numChildrenTriggered;
+    while (i--) {
+      cevents = children[i].events || (children[i].events = {});
+      ce = cevents[event] || (cevents[event] = {});
+      parents = cevents[event].parents || (cevents[event].parents = []);
+      parents.push(this); // TODO Double counting check
+    }
+    childs.push.apply(childs, children);
+    e.numChildrenTriggered || (e.numChildrenTriggered = 0);
+    return this;
+  },
+  isTriggered: function (event) {
+    return !!this.events && !!this.events[event] && !!this.events[event].triggered;
+  },
+  trigger: function (event) {
+    if (!this.events) return false;
+    var e = this.events[event]
+      , childs
+      , i, child
+      , parents, parent;
+    if (e.triggered) return false;
+
+    // Check that all children have been triggered
+    if (childs = e.children) {
+      i = childs.length;
+      while (i--) {
+        child = childs[i];
+        if (!child.events[event] || !child.events[event].triggered) {
+          // All children must be triggered before I can be triggered
+          return false;
+        }
+      }
+    }
+    // Mark myself as triggered
+    e.triggered = true;
+
+    // Run my callbacks
+    e.callbackArgs = Array.prototype.slice.call(arguments, 1);
+    if (e.callbacks) for (var j = 0, l = e.callbacks.length; j < l; j++) {
+      e.callbacks[j][0].apply(e.callbacks[j][1], e.callbackArgs)
+    }
+
+    // Notify my parents that I've been triggered
+    parents = e.parents;
+    if (parents) {
+      i = parents.length;
+      while (i--) {
+        parent = parents[i];
+        e = parent.events[event];
+        e.numChildrenTriggered++;
+        if (e.numChildrenTriggered === e.children.length) {
+          parent.trigger(event);
+        }
+      }
+    }
+    return true;
+  },
+  on: function (event, fn, scope) {
+    if (this.isTriggered(event)) return fn.apply(scope, this.events[event].callbackArgs);
+    else {
+      this.events || (this.events = {});
+      this.events[event] || (this.events[event] = {});
+      this.events[event].callbacks || (this.events[event].callbacks = []);
+      this.events[event].callbacks.push([fn, scope]);
+    }
+  }
+};
---
+++ test.js
@@ -0,0 +1,148 @@
+var should = require('should')
+  , dependencyPromise = require('./dependency_promise');
+
+function Node () {}
+
+for (var k in dependencyPromise) {
+  Node.prototype[k] = dependencyPromise[k];
+}
+
+module.exports = {
+  'a parent should not be able to trigger if any of its children are untriggered': function () {
+    var parent = new Node()
+      , childOne = new Node()
+      , childTwo = new Node();
+    parent.dependsOn('loaded', [childOne, childTwo]);
+    parent.trigger('loaded').should.be.false;
+  },
+
+  'a parent should automatically trigger itself once its last child has been triggered': function () {
+    var parent = new Node()
+      , childOne = new Node()
+      , childTwo = new Node();
+    parent.dependsOn('loaded', [childOne, childTwo]);
+    childOne.trigger('loaded');
+    childOne.isTriggered('loaded').should.be.true;
+    childTwo.trigger('loaded');
+    childTwo.isTriggered('loaded').should.be.true;
+    parent.isTriggered('loaded').should.be.true;
+  },
+
+  'a parent should not automatically trigger itself until all children have been triggered': function () {
+    var parent = new Node()
+      , childOne = new Node()
+      , childTwo = new Node();
+    parent.dependsOn('loaded', [childOne, childTwo]);
+    childOne.trigger('loaded');
+    childOne.isTriggered('loaded').should.be.true;
+    childTwo.isTriggered('loaded').should.be.false;
+    parent.isTriggered('loaded').should.be.false;
+  },
+
+  'triggering should be transitive': function () {
+    var grandpa = new Node()
+      , mama = new Node()
+      , kid = new Node();
+    grandpa.dependsOn('loaded', [mama]);
+    mama.dependsOn('loaded', [kid]);
+    kid.trigger('loaded');
+    kid.isTriggered('loaded').should.be.true;
+    mama.isTriggered('loaded').should.be.true;
+    grandpa.isTriggered('loaded').should.be.true;
+  },
+
+  'assigned callbacks should be invoked by a trigger': function () {
+    var parent = new Node()
+      , child = new Node()
+      , counter = 0;
+    parent.on('loaded', function () {
+      counter++;
+    });
+    parent.dependsOn('loaded', [child]);
+    child.trigger('loaded');
+    counter.should.equal(1);
+  },
+
+  'assigned callbacks should run immediately if the event has already been triggered': function () {
+    var parent = new Node()
+      , child = new Node()
+      , counter = 0;
+    parent.dependsOn('loaded', [child]);
+    child.trigger('loaded');
+    parent.on('loaded', function () {
+      counter++;
+    });
+    counter.should.equal(1);
+  },
+
+  'triggering should be able to pass along arguments to callbacks': function () {
+    var parent = new Node()
+      , child = new Node()
+      , counter = 0;
+    parent.dependsOn('loaded', [child]);
+    child.on('loaded', function (a, b) {
+      counter = a + b;
+    });
+    child.trigger('loaded', 5, 2);
+    counter.should.equal(7);
+  },
+
+  'arguments passed to a trigger should be remembered and passed to callbacks declared after a trigger': function () {
+    var parent = new Node()
+      , child = new Node()
+      , counter = 0;
+    parent.dependsOn('loaded', [child]);
+    child.trigger('loaded', 5, 2);
+    child.on('loaded', function (a, b) {
+      counter = a + b;
+    });
+    counter.should.equal(7);
+  },
+
+  'callbacks can be associated with a scope': function () {
+    var parent = new Node()
+      , child = new Node()
+      , counter = 0;
+    child.on('loaded', function (a, b) {
+      counter = a + b + this.y + this.z;
+    }, {y: 100, z: 50});
+    parent.dependsOn('loaded', [child]);
+    child.trigger('loaded', 5, 2);
+    counter.should.equal(157);
+  },
+
+  'triggering twice should not trigger callbacks twice': function () {
+    var parent = new Node()
+      , child = new Node()
+      , counter = 0;
+    parent.dependsOn('loaded', [child]);
+    parent.on('loaded', function () {
+      counter++;
+    });
+    child.trigger('loaded').should.be.true;
+    counter.should.equal(1);
+    child.trigger('loaded').should.be.false;
+    counter.should.equal(1);
+  },
+
+  'test transitive relationship with callbacks': function () {
+    var grandpa = new Node()
+      , mama = new Node()
+      , kid = new Node()
+      , counter = 0;
+    grandpa.on('loaded', function () {
+      counter++;
+    });
+    mama.on('loaded', function () {
+      counter++;
+    });
+    kid.on('loaded', function () {
+      counter++;
+    });
+
+    grandpa.dependsOn('loaded', [mama]);
+    mama.dependsOn('loaded', [kid]);
+    kid.trigger('loaded');
+    counter.should.equal(3);
+  }
+};
